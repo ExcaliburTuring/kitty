3a6a7bfd241add4423bf1952d4eb24c088c1bee8
zhenzong <zhenzong.peng@icloud.com>
2016-04-18 23:23:54 +0800

    各种重命名

---
+++ src/register/js/wxregister.jsx
@@ -0,0 +1,26 @@
+/**
+ * @author xiezhenzong
+ */
+import React from 'react';
+
+import { defaultValue } from 'config';
+
+var WxRegister = React.createClass({
+
+    getInitialState: function() {
+        return {
+            'data': {
+            }
+        }
+    },
+
+    render: function() {
+        return (
+            <div> 
+                <p>这里是微信登录的二维码</p>
+            </div>
+        );
+    }
+});
+
+module.exports = WxRegister;
